470d95a49ff127c422d7636dd7a4a063660d239a
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-02-25 02:45:35 -0800

    remove tests

--- tests/sass-test.js
+++
@@ -1,9 +0,0 @@
-var sass   = require('../lib/sass.js');
-
-sass.compile('./style.scss', function (css) {
-    console.log(css);
-});
-
-sass.compile('./style.scss', {'outputStyle':'expanded'}, function (css) {
-    console.log(css);    
-});
